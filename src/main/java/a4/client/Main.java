edb576ccdd4b09d190f5929feaa4184de3c31314
hornerj <hornerj@INF-9-007-06.windows.reutlingen-university.de>
2018-11-10 16:27:31 +0100

    Update Client

--- src/main/java/a4/client/Main.java
+++ src/main/java/a4/client/Main.java
@@ -1,5 +1,10 @@
 package a4.client;
 
+import java.util.Random;
+
+import a4.ClientType;
+import a4.VoteType;
+
 public class Main {
     // ---------------------------------------------------------------------------------------------
 
@@ -12,14 +17,29 @@ public class Main {
     // ---------------------------------------------------------------------------------------------
 
     public static void main(String[] args) {        
-        ClientCommunicator clientCommunicator = new ClientCommunicator();
-        clientCommunicator.setCommunicationParameters(SERVER, PORT);
-        Client client = new Client(clientCommunicator);
+        for (int i = 0; i < 323; i++) {
+            ClientCommunicator clientCommunicator = new ClientCommunicator();
+            clientCommunicator.setCommunicationParameters(SERVER, PORT);
+
+            Client client = new Client(clientCommunicator);
+            client.setClientType(ClientType.VOTING_CLIENT);
+            client.setTopic("Bananen");
+            client.setVoteType(VoteType.values()[new Random().nextInt(3)]);
+            client.start();
+
+        }
         
-        client.addTopic("Bananen");
-        client.checkVotes("Bananen");
-        
-        clientCommunicator.closeConnection();
+        ClientCommunicator clientResultCommunicator = new ClientCommunicator();
+        clientResultCommunicator.setCommunicationParameters(SERVER, PORT);
+        Client resultClient = new Client(clientResultCommunicator);
+        resultClient.setClientType(ClientType.CURRENT_STATE_CLIENT);
+        resultClient.setTopic("Bananen");
+        resultClient.start();
+        try {
+			resultClient.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
     }
     
     // ---------------------------------------------------------------------------------------------
