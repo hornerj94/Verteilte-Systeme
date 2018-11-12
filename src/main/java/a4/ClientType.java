9115180b8114020066ce563c368bb84e6140e859
Julian Honer <julianhorner@web.de>
2018-11-12 09:01:29 +0100

    Update client part of the application in task 4

--- src/main/java/a4/ClientType.java
+++ src/main/java/a4/ClientType.java
@@ -9,11 +9,38 @@ package a4;
 public enum ClientType {
     // ---------------------------------------------------------------------------------------------
     
-    VOTING_CLIENT,
+    VOTING_CLIENT("Vote"),
     
-    CURRENT_STATE_CLIENT,
+    CURRENT_STATE_CLIENT("Result"),
     
-    NEW_TOPIC_CLIENT;
+    NEW_TOPIC_CLIENT("Topic");
     
+    // ---------------------------------------------------------------------------------------------
+    
+    /** The name of the value. */
+    private final String name;
+
+    // ---------------------------------------------------------------------------------------------
+
+    /**
+     * Creates an enum value with the given parameter.
+     * 
+     * @param name The name of the enum.
+     */
+    private ClientType(final String name) {
+        this.name = name;
+    }
+    
+    // ---------------------------------------------------------------------------------------------
+
+    /**
+     * Gets the name of the value.
+     * 
+     * @return The name of the value
+     */
+    public String getName() {
+        return name;
+    }
+
     // ---------------------------------------------------------------------------------------------
 }
--- src/main/java/a4/client/Main.java
+++ src/main/java/a4/client/Main.java
@@ -1,8 +1,6 @@
 package a4.client;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.Scanner;
 
 import a4.ClientType;
 import a4.VoteType;
@@ -16,43 +14,133 @@ public class Main {
     /** The port of the server to send the incoming messages. */
     private final static String SERVER = "DESKTOP-Q99AK62.fh-reutlingen.de";
 
+    /** The scanner for reading the users input. */
+    private final static Scanner scanner = new Scanner(System.in);
+
+    /** The current type of the client.  */
+    private static ClientType clientType = null;
+    
+    /** The current type of the vote.  */
+    private static VoteType voteType = null;
+
+    /** The current topic of the client.  */
+    private static String topic = "";
+
     // ---------------------------------------------------------------------------------------------
 
-    public static void main(String[] args) {
-        List<Client> clients = new ArrayList<>();
-        String topic = "Bananen";
-        
-        // Creates client threads that vote with an random vote type for the topic.
-        for (int i = 0; i < 323; i++) {
-            ClientCommunicator clientCommunicator = new ClientCommunicator(SERVER, PORT);
+    public static void main(String[] args) { 
+        while (true) {
+            startConsole();
 
-            VoteType randomVoteType = VoteType.values()[new Random().nextInt(3)];
-            Client client = new Client(clientCommunicator, ClientType.VOTING_CLIENT, topic, 
-                    randomVoteType);
-            clients.add(client);
+            ClientCommunicator clientCommunicator = new ClientCommunicator(SERVER, PORT);
+            Client client = 
+                    new Client(clientCommunicator, ClientType.VOTING_CLIENT, topic, voteType);
             client.start();
-            
-        }
-        
-        // Wait till the threads are done.
-        for (Client client : clients) {
+
             try {
                 client.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        
-        // Get the results for the topic.
-        ClientCommunicator clientResultCommunicator = new ClientCommunicator(SERVER, PORT);
-        Client resultClient = new Client(clientResultCommunicator, ClientType.VOTING_CLIENT, topic);
-        resultClient.start();
-        try {
-			resultClient.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
     }
-    
+
+    // ---------------------------------------------------------------------------------------------
+
+    private static void startConsole() {
+
+        System.out.println("Sie haben folgende Auswahlmöglichkeiten:\n");
+        System.out.println("Geben sie das Wort \"Vote\" ein, " 
+                        + "um für ein Thema das sie interessiert eine Stimme abzugeben ");
+        System.out.println("Geben sie das Wort \"Topic\" ein, " 
+                    + "um ein neues Thema erstellen ");
+        System.out.println("Geben sie das Wort \"Result\" ein, " 
+                + "um die gesammelten Ergebnisse eines Themas zu betrachten ");
+        System.out.println("Geben sie das Wort \"Exit\" ein, " 
+                + "um das Programm zu beenden ");
+        System.out.println("\nEingabe: ");
+
+        String input = scanner.nextLine();
+
+        switch (input) {
+        case "Vote":
+            vote();
+            clientType = ClientType.VOTING_CLIENT;
+            break;
+
+        case "Topic":
+            topic();
+            clientType = ClientType.NEW_TOPIC_CLIENT;
+            break;
+
+        case "Result":
+            result();
+            clientType = ClientType.CURRENT_STATE_CLIENT;
+            break;
+
+        case "Exit":
+            System.exit(0);
+            break;
+            
+        default:
+            System.out.println("Ungültige Eingabe");
+            break;
+        }
+    }
+
+    private static void vote() {
+        while (true) {
+            System.out.println("\nGeben sie das Thema ein zu dem sie eine Stimme abgeben wollen:");
+            System.out.println("Thema: ");
+            topic = scanner.nextLine();
+            
+            System.out.println("Geben sie \"Approval\" ein, "
+                    + "wenn sie dem Thema ihr Zustimmung geben wollen");
+            System.out.println("Geben sie \"Refusal\" ein, "
+                    + "wenn sie das Thema Ablehnen wollen");
+            System.out.println("Geben sie \"Abstention\" ein, "
+                    + "wenn sie sich bei dem Thema enthalten wollen");
+            System.out.println("Stimme: ");
+
+            voteType = convertToEnum(scanner.nextLine());
+            if (voteType == null) {
+                System.out.println("Ungültige Eingabe\n");
+            } else {
+                break;
+            }
+        }
+    }
+
+    private static void topic() {
+        System.out.println("\nGeben sie den Namen des Themas ein das sie erstellen wollen:");
+        System.out.println("Thema: ");
+        topic = scanner.nextLine();
+    }
+
+    private static void result() {
+        while (true) {
+            System.out.println("\nGeben sie den Namen des Themas ein,"
+                    + " von welchem sie die gesammelten Ergebnisse betrachten wollen:");
+            System.out.println("Thema: ");
+            topic = scanner.nextLine();
+
+        }
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    private static VoteType convertToEnum(final String voteType) {
+        switch (voteType) {
+        case "Approval":
+            return VoteType.APPROVAL;
+        case "Refusal":
+            return VoteType.REFUSAL;
+        case "Abstention":
+            return VoteType.ABSTENTION;
+        default:
+            return null;
+        }
+    }
+
     // ---------------------------------------------------------------------------------------------
 }
